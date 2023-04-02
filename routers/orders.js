a7697e6926d20e8e6a4346201e9deed68e01798d
gonz <gonzasestopal@gmail.com>
2023-04-01 21:50:58 -0600

    feat: verify working hours and confirm order

--- routers/orders.js
+++ routers/orders.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const orders = require('../models/orders.js');
+const meals = require('../models/meals.js');
 const {
     validateItemId,
     validatePostReqBody,
@@ -21,7 +22,6 @@ router.get('/', isAdmin, (req, res) => {
         })
         .catch(err => {
             res.status(500).json({ message: 'Error retrieving the orders.' })
-            console.log(err)
         })
 })
 
@@ -33,13 +33,50 @@ router.get('/:id', isAdmin, validateItemId, (req, res) => {
         })
         .catch(err => {
             res.status(500).json({ message: 'Error retrieving the item.' })
-            console.log(err)
         })
 })
 
-router.post('/', verifyToken, validatePostReqBody, (req, res) => {
-    const item = req.body
-    orders.add(item)
+router.post('/', verifyToken, (req, res) => {
+    const meal_id = req.body.meal_id
+
+    const user = req.user
+
+    if (!(meal_id)) {
+        res.status(400).send("All input is required");
+        return;
+    }
+
+    if (user.status == 'inactive') {
+        res.status(403).json({ message: 'Invalid user status.' });
+        return;
+    }
+
+    opening = new Date()
+    opening.setHours('10')
+
+    closing = new Date()
+    closing.setHours('22')
+
+    now = new Date()
+
+    if (opening >= now >= closing) {
+        res.status(403).json({ message: 'We are not accepting orders.' });
+        return;
+    }
+
+    meals.findById(meal_id)
+        .then(item => {
+            if (!item.is_available) {
+                throw new InvalidStatus()
+            }
+        })
+        .then(_ => {
+            return orders.add({
+                meal_id: meal_id,
+                user_id: user.id,
+                status: 'added'
+            })
+        })
         .then(id => {
             [newItemId] = id
             return orders.findById(newItemId['id'])
@@ -48,16 +85,52 @@ router.post('/', verifyToken, validatePostReqBody, (req, res) => {
             res.status(201).json({ message: 'Successfully added the item.', item })
         })
         .catch(err => {
+            if (err.name == 'InvalidStatus') {
+                res.status(403).json({ message: 'Invalid meal status.' });
+                return;
+            }
             res.status(500).json({ message: 'Error adding the item.' })
         })
 })
+
+router.put('/:id/confirm', verifyToken, (req, res) => {
+    const order_id = req.body.order_id
+
+    if (!(order_id)) {
+        res.status(400).send("All input is required");
+        return;
+    }
+
+    orders.findById(order_id)
+        .then(item => {
+            if (item.status !== 'added') {
+                throw new InvalidStatus()
+            }
+            return orders.edit(item.id, { 'status': 'confirmed' })
+        })
+        .then(updatedItemId => {
+            [updatedItemId] = updatedItemId
+            return orders.findById(updatedItemId['id'])
+        })
+        .then(updated => {
+            res.status(201).json(updated)
+        })
+        .catch(err => {
+            if (err.name == 'InvalidStatus') {
+                res.status(401).json({ message: 'Order cannot be confirmed.' })
+                return;
+            }
+            res.status(500).json({ message: 'Error updating the item.' })
+        })
+
+})
+
 
 router.put('/:id', isAdmin, validateItemId, (req, res) => {
     const id = req.params.id
     const updated = req.body
     orders.findById(id)
         .then(item => {
-            console.log(item.status)
             if (item.status === 'preparando') {
                 throw new InvalidStatus()
             }
